d3cf17f495c587920e6723ffac28f799a992fc17
akirakak <akirakakar2@gmail.com>
2018-11-10 17:52:32 -0500

    setup json files for eslint and vscode

--- react-native/react.sh
+++ react-native/react.sh
@@ -1,5 +1,9 @@
+current=`pwd`
 cd $1
 react-native init $2
 npm -S install eslint
 eslint --init
 yarn add prop-types eslint babel-eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-ally elint-plugin-react prettier prettier-eslint react-native-extended-stylesheet color moment
+cp $current/eslint.json `pwd`/.eslintrc.json
+mkdir $current/.vscode
+cp $current/.vscode/settings.json `pwd`/.vscode/settings.json
