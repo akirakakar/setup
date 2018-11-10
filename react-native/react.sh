5242964e736c533ff37ada52d541584dc39060ce
akirakak <akirakakar2@gmail.com>
2018-11-10 17:23:32 -0500

    react native setup for new project

---
+++ react-native/react.sh
@@ -0,0 +1,5 @@
+cd $1
+react-native init $2
+npm -S install eslint
+eslint --init
+yarn add prop-types eslint babel-eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-ally elint-plugin-react prettier prettier-eslint react-native-extended-stylesheet color moment
